85a1b0d68ada0c63ae11b6ae3c44809e6b4a0683
Vishal_Punzuri <vishalmudhiraj71@gmail.com>
2020-02-15 22:45:28 +0530

    route handlers with query parameters

---
+++ hometask2/task2b.js
@@ -0,0 +1,58 @@
+/**
+ * We are trying to build APIs for a bookstore. Each book should have valid properties as mentioned below
+ *  -	Use fs module wherever necessary
+ *  
+ * 	Book name - string
+ * 	ISBN – 	International Standard Book Number - string
+ * 	Publisher Name - string
+ * 	Author Name - string
+ * 	Author ID - number
+ * 	Price - number
+ * 	Published year - number
+ * 	Reviewers – Array
+ * 	Number of pages: number
+ * 	Country: string
+ * 
+ * Create a JSON file with 10 books and call it as books.json
+
+ * Implement the below endpoints – all must have proper HTTP error codes, exception handling
+
+ * /books – this is a GET call to retrieve all books information
+ * /books/:id – this should retrieve a single book info from the book database (id should refer ISBN in schema)
+ * /books?year=2019 – this should retrieve all books published in 2019
+ * /books?pricestart=<number>&priceend=<number> - this should retrieve all books between the specified price 
+ */
+
+
+var express = require("express");
+var fs = require("fs");
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log("server listening at port ", port);
+});
+
+app.get("/books", (req, res) => {
+    const yearToFind = req.query.year;
+    console.log(yearToFind);
+    var booksData = JSON.parse(fs.readFileSync("books.json", 'utf8'));
+    if (yearToFind) {
+        booksData = booksData.filter(({Published_year}) => yearToFind === Published_year);
+    }
+    res.json(booksData);
+    res.end();
+});
+
+app.get("/books/:id", (req, res) => {
+    const id = req.params.id;
+    const booksData = JSON.parse(fs.readFileSync("books.json", 'utf8'));
+    res.json(booksData.find(({ISBN}) => id === ISBN));
+    res.end();
+});
